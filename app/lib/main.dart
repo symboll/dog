4667a0561940e7da4b959e898424fe26ae892d55
likanghua <lkh19890913@163.com>
2020-10-25 15:28:02 +0800

    feat: add app

---
+++ app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('App'),
+        //   elevation: 0,
+        // ),
+        body: Center(
+          child: Text('',
+            textDirection: TextDirection.ltr,
+          ),
+        )
+      ),
+    );
+  }
+}
--- pc/src/store/reducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers  } from 'redux'
-
-import { reducer as HomeReducer } from '../pages/Home/store'
-
-export default combineReducers({
-  Home: HomeReducer
-})
